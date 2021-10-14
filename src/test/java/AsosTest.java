b8f4d1656bd208d79ce4db1cb01f136a822e6b80
Yelena03 <freya0334@gmail.com>
2021-10-14 01:13:23 -0400

    Changed class name

--- src/test/java/AsosTest.java
+++ src/test/java/AsosTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class SheinAutoTests {
+public class AsosTest {
 
     private WebDriver driver;
     String url = "https://www.asos.com/us/";
