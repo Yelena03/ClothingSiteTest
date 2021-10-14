f8659697f6766e43213630221ed95651e8842ca4
Yelena03 <freya0334@gmail.com>
2021-10-13 23:02:42 -0400

    needs to be reviewed

---
+++ src/test/java/SheinAutoTests.java
@@ -0,0 +1,64 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SheinAutoTests {
+
+    private WebDriver driver;
+    String url = "https://www.asos.com/us/";
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+        @AfterMethod
+        public void setDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void urlVerify() throws InterruptedException {
+
+        String expectedResult = "https://www.asos.com/us/";
+
+        Thread.sleep(3000);
+
+        String actualResult = driver.getCurrentUrl();
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+    @Test
+    public void searchInput(){
+        WebElement searchBar = driver.findElement(By.xpath("//input[@type='search']"));
+
+        searchBar.sendKeys("skirt\n");
+
+        String originalWindow = driver.getWindowHandle();
+        assert driver.getWindowHandles().size() != 1;
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        for (String windowHandle : driver.getWindowHandles()) {
+            if(!originalWindow.contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        WebElement confirmText = driver.findElement(By.xpath("//p[contains(text(),'skirt')]"));
+        String confirmTextText = confirmText.getText();
+        Assert.assertEquals(confirmTextText, "Skirt");
+    }
+}
